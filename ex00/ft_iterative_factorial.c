b1512e9767840a20974946c43aa64ab139fb110c
Nkosinathi Radebe <nkradebe@c5r8s3.wethinkcode.co.za>
2020-07-12 11:54:12 +0200

    day04 ex04

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: nkradebe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 11:21:23 by nkradebe          #+#    #+#             */
-/*   Updated: 2020/07/12 11:41:00 by nkradebe         ###   ########.fr       */
+/*   Updated: 2020/07/12 11:53:54 by nkradebe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ int		ft_iterative_factorial(int	nb)
 
 	i	=	0;
 	factorial_results	=	0;
+	if(!nb)
+		return(0);
+
 	while(i	<=	nb)
 	{
 		factorial_results = factorial_results * i;
