cb11e45f9dca103a6917119c6b648c80151d762c
Nkosinathi Radebe <nkradebe@c5r8s3.wethinkcode.co.za>
2020-07-12 12:31:50 +0200

    day04 ex01

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -1,30 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkradebe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/12 11:21:23 by nkradebe          #+#    #+#             */
-/*   Updated: 2020/07/12 12:22:09 by nkradebe         ###   ########.fr       */
+/*   Created: 2020/07/12 11:45:56 by nkradebe          #+#    #+#             */
+/*   Updated: 2020/07/12 12:30:58 by nkradebe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_iterative_factorial(int	nb)
+int		ft_recursive_factorial(int	nb)
 {
-	int		i;
 
-	int factorial_results;
-
-	i	=	1;
-	factorial_results	=	1;
 	if(nb	<	0)
 		return(0);
 
-	while(i	<=	nb)
-	{
-		factorial_results = factorial_results * i;
-		i++;
-	}
-	return(factorial_results);
+		return nb*ft_recursive_factorial(nb - 1);
 }
